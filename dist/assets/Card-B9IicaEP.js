5c11963e5d8363a5fcbd124cdce76fdd9f7634f4
y4ch0 <82388185+y4ch0@users.noreply.github.com>
2025-01-25 20:18:20 +0100

    add dist

--- dist/assets/Card-B9IicaEP.js
+++
@@ -1,6 +0,0 @@
-import{a as s}from"./ComponentList-CAppzH0k.js";import{a,c as e,b as i,d as o,F as d,o as n}from"./index-BI-tLvRI.js";const u={__name:"Card",setup(c){return(r,t)=>(n(),a(d,null,[t[0]||(t[0]=e(`<div class="rows"><div class="row"><h1 id="description">Card</h1><p>Cards contain content and actions about a single subject.</p><details open><summary>Component preview</summary><div class="details-content"><div class="card"><h2>Lorem ipsum dolor sit amet.</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde iure ipsam sint, culpa adipisci quam.</p><button class="button secondary">Read</button></div></div></details></div><div class="row"><details><summary>Component code</summary><div class="details-content"><div class="code-block"><pre><code>&lt;div class=&quot;card&quot;&gt;
-    &lt;h2&gt;Lorem ipsum dolor sit amet.&lt;/h2&gt;
-    &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde iure ipsam 
-    sint, culpa adipisci quam.&lt;/p&gt;
-    &lt;button class=&quot;button secondary&quot;&gt;Read&lt;/button&gt;
-&lt;/div&gt;</code></pre></div></div></details></div></div>`,1)),i("aside",null,[o(s)])],64))}};export{u as default};
