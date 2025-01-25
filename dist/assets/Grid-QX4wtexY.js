64fb02847daeb63650dd031cf1f84ac036d2d15a
y4ch0 <82388185+y4ch0@users.noreply.github.com>
2025-01-25 20:28:58 +0100

    .

---
+++ dist/assets/Grid-QX4wtexY.js
@@ -0,0 +1,9 @@
+import{a as s}from"./ComponentList-xsq8_Z4X.js";import{a as t,c as i,b as l,d,F as a,o as c}from"./index-DlDtFXkU.js";const v={__name:"Grid",setup(e){return(n,o)=>(c(),t(a,null,[o[0]||(o[0]=i(`<div class="rows"><div class="row"><h1 id="description">Grid</h1><p>Grid layout is created to help your webiste look organized. The grid is mobile-first, powered by flexbox.</p><div class="alert warning"><div class="icon"><i class="bi bi-info-circle-fill"></i></div><div class="data"><h3>Pay attention to layout.</h3><p>Do not use more than 6 columns due to possible layout issues which may be difficult to read.</p></div></div><details open><summary>Component preview</summary><div class="details-content"><div class="docs-rows"><div class="row-docs"> Single row, without columns </div><div class="row-docs"><div class="column-docs">Column</div><div class="column-docs">Column</div><div class="column-docs">Column</div></div></div></div></details></div><div class="row"><details><summary>Component code</summary><div class="details-content"><div class="code-block"><pre><code>&lt;div class=&quot;row&quot;&gt;
+    row without columns
+&lt;/div&gt;
+&lt;div class=&quot;row&quot;&gt;
+    &lt;div class=&quot;column&quot;&gt;column&lt;/div&gt;
+    &lt;div class=&quot;column&quot;&gt;column&lt;/div&gt;
+    &lt;div class=&quot;column&quot;&gt;column&lt;/div&gt;
+    &lt;div class=&quot;column&quot;&gt;column&lt;/div&gt;
+&lt;/div&gt;</code></pre></div></div></details></div></div>`,1)),l("aside",null,[d(s)])],64))}};export{v as default};
