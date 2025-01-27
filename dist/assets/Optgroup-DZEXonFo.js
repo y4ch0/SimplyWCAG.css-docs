73690724215818d887d5032027459c676752f5d6
Hubnew <hubertjankowi1@gmail.com>
2025-01-27 21:37:15 +0100

    add dist files

---
+++ dist/assets/Optgroup-DZEXonFo.js
@@ -0,0 +1,12 @@
+import{a as t}from"./ComponentList-JKI6FIIF.js";import{a as e,c as l,b as a,d as s,F as i,o as p}from"./index-B8f8afAW.js";const c={__name:"Optgroup",setup(r){return(d,o)=>(p(),e(i,null,[o[0]||(o[0]=l(`<div class="rows"><div class="row"><h1 id="description">Optgroup</h1><p>A optgroup is used to group related options in a <code>&lt;select&gt;</code> element (drop-down list).</p><details open><summary>Component preview</summary><div class="details-content"><div class="form-group"><select class="form-control"><optgroup label="Swedish Cars"><option value="volvo">Volvo</option><option value="saab">Saab</option></optgroup><optgroup label="German Cars"><option value="mercedes">Mercedes</option><option value="audi">Audi</option></optgroup></select></div></div></details><div class="alert information slim"><p>To let user select multiple options add <code>multiple</code> tag at <code>&lt;select&gt;</code></p></div></div><div class="row"><details><summary>Component code</summary><div class="details-content"><div class="code-block"><pre><code>&lt;div class=&quot;form-group&quot;&gt;
+    &lt;select class=&quot;form-control&quot;&gt;
+        &lt;optgroup label=&quot;Swedish Cars&quot;&gt;
+            &lt;option value=&quot;volvo&quot;&gt;Volvo&lt;/option&gt;
+            &lt;option value=&quot;saab&quot;&gt;Saab&lt;/option&gt;
+        &lt;/optgroup&gt;
+        &lt;optgroup label=&quot;German Cars&quot;&gt;
+            &lt;option value=&quot;mercedes&quot;&gt;Mercedes&lt;/option&gt;
+            &lt;option value=&quot;audi&quot;&gt;Audi&lt;/option&gt;
+        &lt;/optgroup&gt;
+    &lt;/select&gt;
+&lt;/div&gt;</code></pre></div></div></details></div></div>`,1)),a("aside",null,[s(t)])],64))}};export{c as default};
